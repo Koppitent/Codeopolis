bf3aec19d0b483e7513ae38f9814e1456b7fa472
koppitent <nicoeinstein@hotmail.com>
2023-12-04 11:13:20 +0100

    bugfixes

--- src/main/java/de/koppy/Main.java
+++ src/main/java/de/koppy/Main.java
@@ -2,6 +2,7 @@ package de.koppy;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Program starts here!");
         CityTestDrive.startTest();
     }
 }
